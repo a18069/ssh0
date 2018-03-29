c0dd740c75f02985c149a674f4a9e76053b08edb
hostwin <1659702093@qq.com>
2018-03-30 00:41:07 +0800

    Initial commit

---
+++ src/main/java/com/jbit/test/Test.java
@@ -0,0 +1,10 @@
+package com.jbit.test;
+
+/**
+ * Created by hostwin on 2018/3/30.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(1234);
+    }
+}
